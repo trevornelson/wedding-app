d8ead10c6e049e9c56b3a6dfbb4fa6e80f007840
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-11 11:01:56 -0400

    Added descriptions for planned tests

---
+++ spec/models/gift.rb
@@ -0,0 +1,14 @@
+require_relative '../spec_helper'
+
+describe 'Gift Model' do
+  let(:gift) { Gift.new }
+
+  it 'saved a valid gift to the database' do
+
+  end
+
+  it 'flagged gift as purchased' do
+
+  end
+
+end
---
+++ spec/models/user.rb
@@ -0,0 +1,29 @@
+require_relative '../spec_helper'
+
+describe 'User Model' do
+  let(:user) { User.new }
+
+  it 'saved a valid user to the database' do
+
+  end
+
+  it 'flagged user as not attending' do
+
+  end
+
+  it 'flagged a user as attending' do
+
+  end
+
+  it 'did not save a user to the database with an invalid type attribute' do
+
+  end
+
+  it 'encrypted a users password' do
+
+  end
+
+  it 'did not save a user to the database with a non-unique username' do
+
+  end
+end
