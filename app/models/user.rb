7ca344e3474c2492547004d018bf742b9a157c72
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-11 09:55:41 -0400

    Added validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ActiveRecord::Base
+
+  validates :type, inclusion: { in: %w(Admin Attending NotAttending NoResponse),
+    message: "Input is not a valid user type." }
+
+  validates :username, uniqueness: true
+
+  validates_presence_of :type,
+                        :username,
+                        :password_digest,
+                        :first_name,
+                        :last_name,
+
+  validates_inclusion_of :invited_plus_one
 end
