52c0c5f56d3596ce0c80addf8c574919e04b9205
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-11 09:20:41 -0400

    Finished CreateNotes migration

--- db/migrations/20150311090652_create_notes.rb
+++ db/migrations/20150311090652_create_notes.rb
@@ -1,4 +1,11 @@
 class CreateNotes < ActiveRecord::Migration
   def change
+    create_table :notes do |t|
+      t.integer   :prompt_id, null: false
+      t.integer   :user_id, null: false
+      t.text      :content, null: false
+
+      t.timestamps
+    end
   end
 end
