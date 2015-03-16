c2408fffd61d9f703ad89020d0a9e079717f4912
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-16 16:11:10 -0400

    Added gift_img column migration for gifts table

--- wedding_app/db/schema.rb
+++ wedding_app/db/schema.rb
@@ -11,19 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313144632) do
+ActiveRecord::Schema.define(version: 20150316200919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "gifts", force: :cascade do |t|
-    t.string   "title",                      null: false
-    t.string   "category",                   null: false
+    t.string   "title",                                 null: false
+    t.string   "category",                              null: false
     t.string   "url"
-    t.string   "image_url",                  null: false
-    t.boolean  "purchased",  default: false
+    t.string   "image_url",                             null: false
+    t.boolean  "purchased",             default: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "gift_img_file_name"
+    t.string   "gift_img_content_type"
+    t.integer  "gift_img_file_size"
+    t.datetime "gift_img_updated_at"
   end
 
   create_table "notes", force: :cascade do |t|
